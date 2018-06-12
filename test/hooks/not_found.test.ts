7d6dd81dc1a7896a8fa07ffb8227e1f27bf967b2
RasPhilCo <capt.pn@gmail.com>
2018-06-12 16:58:34 -0700

    feat: did you mean (#21)

--- test/hooks/not_found.test.ts
+++ test/hooks/not_found.test.ts
@@ -1,13 +1,36 @@
 import chalk from '@heroku-cli/color'
-import {test} from '@oclif/test'
+import {expect, test} from '@oclif/test'
+import {cli} from 'cli-ux'
 
 chalk.enabled = false
 
-describe('command', () => {
+describe('command_not_found', () => {
   test
-  .hook('command_not_found', {id: 'hel'})
-  .catch(`hel is not a @oclif/plugin-not-found command.
-Perhaps you meant help
-Run @oclif/plugin-not-found help for a list of available commands.`)
-  .end('runs hook')
+  .stub(cli, 'prompt', () => async () => 'y')
+  .stub(process, 'argv', [])
+  .stdout()
+  .stderr()
+  .hook('command_not_found', {id: 'commans'})
+  .catch('EEXIT: 0')
+  .end('runs hook with suggested command on yes', (ctx: any) => {
+    expect(ctx.stderr).to.be.contain('Warning: commans is not a @oclif/plugin-not-found command.\n')
+    expect(ctx.stdout).to.be.contain('commands\nhelp')
+  })
+
+  test
+  .stderr()
+  .stub(cli, 'prompt', () => async () => 'n')
+  .hook('command_not_found', {id: 'commans'})
+  .catch('Run @oclif/plugin-not-found help for a list of available commands.')
+  .end('runs hook with not found error on no', (ctx: any) => {
+    expect(ctx.stderr).to.be.contain('Warning: commans is not a @oclif/plugin-not-found command.\n')
+  })
+
+  test
+  .stderr()
+  .hook('command_not_found', {id: 'commans'})
+  .catch('Run @oclif/plugin-not-found help for a list of available commands.')
+  .end('runs hook with not found error after no input timeout', (ctx: any) => {
+    expect(ctx.stderr).to.be.contain('Warning: commans is not a @oclif/plugin-not-found command.\n')
+  })
 })
