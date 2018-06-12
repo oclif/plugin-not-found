71b68e00306593fd581e4e3e84ac392bcd59867e
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-06-12 16:41:49 -0700

    feat: did you mean

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {color} from '@heroku-cli/color'
 import {Hook} from '@oclif/config'
+import {cli} from 'cli-ux'
 
 const hook: Hook<'command_not_found'> = async function (opts) {
   const commandIDs = opts.config.commandIDs
@@ -14,16 +15,26 @@ const hook: Hook<'command_not_found'> = async function (opts) {
   if (await opts.config.findTopic(idSplit[0])) {
     // if valid topic, update binHelp with topic
     binHelp = `${binHelp} ${idSplit[0]}`
-    // if topic:COMMAND present, try closest for id
-    // if (idSplit[1]) closest = this.closest(id)
   }
 
-  let perhaps = closest ? `Perhaps you meant ${color.yellow(closest(opts.id))}\n` : ''
-  this.error(
-    `${color.yellow(opts.id)} is not a ${opts.config.bin} command.
-${perhaps}Run ${color.cmd(binHelp)} for a list of available commands.`,
-    {exit: 127},
-  )
+  let suggestion: string = closest(opts.id)
+  this.warn(`${color.yellow(opts.id)} is not a ${opts.config.bin} command.`)
+
+  let response
+  try {
+    response = await cli.prompt(`Did you mean ${suggestion}? [y/n]`, {timeout: 4900})
+  } catch (err) {
+    this.log('')
+    this.debug(err)
+  }
+
+  if (response === 'y') {
+      const argv = process.argv
+      await this.config.runCommand(suggestion, argv.slice(3, argv.length))
+      this.exit(0)
+  }
+
+  this.error(`Run ${color.cmd(binHelp)} for a list of available commands.`, {exit: 127})
 }
 
 export default hook
