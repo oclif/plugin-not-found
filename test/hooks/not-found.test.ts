fad982954ef9af3a52a572f28b3a9ef8eec5fa74
peternhale <peternhale@users.noreply.github.com>
2021-12-07 07:05:02 -0700

    fix: bump deps (#235)
    * fix: bump deps
    
    * chore: fix circle config
    
    * chore: fix eslint erors
    
    * chore: fix github workflow node versions

--- test/hooks/not-found.test.ts
+++ test/hooks/not-found.test.ts
@@ -1,12 +1,14 @@
-import chalk from '@oclif/color'
+import chalk, {color} from '@oclif/color'
 import {expect, test} from '@oclif/test'
 import {cli} from 'cli-ux'
 
-chalk.enabled = false
+color(chalk, 'enabled', 0)
 
 describe('command_not_found', () => {
   test
   .stub(cli, 'prompt', () => async () => 'y')
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
   .stub(process, 'argv', [])
   .stdout()
   .stderr()
@@ -14,14 +16,14 @@ describe('command_not_found', () => {
   .catch('EEXIT: 0')
   .end('runs hook with suggested command on yes', (ctx: any) => {
     expect(ctx.stderr).to.be.contain('Warning: commans is not a @oclif/plugin-not-found command.\n')
-    expect(ctx.stdout).to.be.contain('commands\nhelp')
+    expect(ctx.stdout).to.match(/commands.+?\n.*?help/)
   })
 
   test
   .stderr()
   .stub(cli, 'prompt', () => async () => 'n')
   .hook('command_not_found', {id: 'commans'})
-  .catch('Run @oclif/plugin-not-found help for a list of available commands.')
+  .catch((error: Error) => error.message.includes('Run @oclif/plugin-not-found help for a list of available commands.'))
   .end('runs hook with not found error on no', (ctx: any) => {
     expect(ctx.stderr).to.be.contain('Warning: commans is not a @oclif/plugin-not-found command.\n')
   })
@@ -29,7 +31,7 @@ describe('command_not_found', () => {
   test
   .stderr()
   .hook('command_not_found', {id: 'commans'})
-  .catch('Run @oclif/plugin-not-found help for a list of available commands.')
+  .catch((error: Error) => error.message.includes('Run @oclif/plugin-not-found help for a list of available commands.'))
   .end('runs hook with not found error after no input timeout', (ctx: any) => {
     expect(ctx.stderr).to.be.contain('Warning: commans is not a @oclif/plugin-not-found command.\n')
   })
