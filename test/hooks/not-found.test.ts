3805ddbee8d6f0798a1ff5fe2e28d1a2e369d191
Cristian Dominguez <cdominguez@salesforce.com>
2025-05-29 11:21:14 -0300

    fix: skip prompt if not attached to a TTY

--- test/hooks/not-found.test.ts
+++ test/hooks/not-found.test.ts
@@ -9,6 +9,20 @@ describe('command_not_found', () => {
     sinon.restore()
   })
 
+  it('should not prompt if not attached to a TTY', async () => {
+    sinon.stub(process, 'argv').returns([])
+    sinon.stub(process.stdin, 'isTTY').set(() => false)
+
+    const startTime = Date.now()
+    const {error, stderr} = await runHook('command_not_found', {id: 'commans'})
+    const endTime = Date.now()
+
+    // the prompt times out after 10s, this assertion ensures the prompt never got rendered and the hook failed quickly
+    expect(endTime - startTime).to.be.lessThan(3000)
+    expect(stderr).to.contain('Warning: commans is not a @oclif/plugin-not-found command.\n')
+    expect(error?.message).to.contain('Run @oclif/plugin-not-found help for a list of available commands.')
+  })
+
   it('should run hook with suggested command on yes', async () => {
     sinon.stub(utils, 'getConfirmation').resolves(true)
     sinon.stub(process, 'argv').returns([])
